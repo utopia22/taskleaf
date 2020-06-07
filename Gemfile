0a7dec189726d7e9527ddd40db38d95da8f140f6
sekishinya <sekishinya@example.com>
2020-06-07 19:37:06 +0900

    一覧詳細修正

--- Gemfile
+++ Gemfile
@@ -66,3 +66,4 @@ gem 'slim-rails'
 gem 'html2slim'
 
 gem 'bootstrap'
+gem 'rails-i18n', '~> 5.1' 
--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,14 @@ Bundler.require(*Rails.groups)
 
 module Taskleaf
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+      config.active_record.default_timezone = :local
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+      #　以下の記述を追記する(設定必須)
+      # デフォルトのlocaleを日本語(:ja)にする
+      config.i18n.default_locale = :ja
+
+      config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+      
   end
 end
