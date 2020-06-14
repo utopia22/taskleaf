8b732be8ad0f653c92d733020a856277e7be02b8
sekishinya <sekishinya@example.com>
2020-06-14 21:12:13 +0900

    confirm_new289

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   end
 
  root to: 'tasks#index'
- resources :tasks
+   resources :tasks do
+     post :confirm, action: :confirm_new, on: :new
+   end
 
   get  '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
