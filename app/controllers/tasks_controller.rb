17ece16f1b71052392d204c4fbf90c0376677d3b
sekishinya <sekishinya@example.com>
2020-06-16 21:00:38 +0900

    編集機能完治

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -36,8 +36,9 @@ class TasksController < ApplicationController
   end
 
   def update
-    @task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
+      
+      @task.update!(task_params)
+      redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
   end
 
   def  destroy
@@ -46,6 +47,7 @@ class TasksController < ApplicationController
   end
 
   private
+
   def task_params
     params.require(:task).permit(:name, :description)
   end
@@ -53,4 +55,5 @@ class TasksController < ApplicationController
   def set_task
     @task = current_user.tasks.find(params[:id])
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
     resources :users
   end
 
- root to: 'tasks#index'
-   resources :tasks do
+  root to: 'tasks#index'
+  resources :tasks do
      post :confirm, action: :confirm_new, on: :new
    end
 
