79a10a2828bf8336e94d03d31469d1d35cecca85
sekishinya <sekishinya@example.com>
2020-06-16 22:18:45 +0900

    検索ソート

--- app/models/task.rb
+++ app/models/task.rb
@@ -11,4 +11,12 @@ class Task < ApplicationRecord
     self.name = '名前なし' if name.blank?
   end
 
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
+
 end
